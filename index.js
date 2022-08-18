06c784be73638e17a36c97671ab7bcc352816ec5
beckireby15@gmail.com <beckireby15@gmail.com>
2022-08-18 11:21:16 +0300

    conditionsl statements

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(someValue){
+  if (someValue <= 400){
+    return 'This one is on me!';
+  }
+  else if (someValue > 2000 && someValue < 2500){
+    return 'I will gladly take your thirty bucks.';
+  }
+  else if (someValue > 2500) {
+    return 'No can do.';
+  }
+
+  }
+
+
+function ternaryCheckCity(city){
+  let message = city === 'NYC'? 'Ok, sounds good.': 'No go.';
+  return message;
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
+function switchOnCharmFromTip(tip){
+  switch (tip){
+    case 'generous':
+      return 'Thank you so much.';
+    case 'not as generous':
+      return 'Thank you.';
+      default: 
+      return 'Bye.';
+    }
+  }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
-}
+switchOnCharmFromTip();
